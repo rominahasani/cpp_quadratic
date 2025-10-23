5dddd235bf68e5e20792540559446f7d981c305c
rominahasani <rominahasani8395@gmail.com>
2025-10-23 07:51:34 +0330

    divisor against common denominator 1

---
+++ main10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,i;
+    cout << "Enter first number:";
+    cin >> a;
+    cout << "Enter second number:";
+    cin >> b;
+    int miny;
+    miny=min(a,b);
+    for (int i=1;i<=miny;i++)
+    {
+        if (a%i==0 && b%i==0)
+            cout << i << endl;
+    }
+    return 0;
+}
