72ac7f892c9b617f489bccb92426405bc94746e6
rominahasani <rominahasani8395@gmail.com>
2025-10-23 07:22:34 +0330

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,1 @@
-#include <iostream>
 
-using namespace std;
-
-int main()
-{
-    float a,b,c;
-    cout << "Enter a:";
-    cin >> a;
-    cout << "Enter b:";
-    cin >> b;
-    if (a==0)
-        cout<<"Error!";
-    else
-        cout<<"x="<<-b/a;
-    return 0;
-}
